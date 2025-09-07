8121bf7b625e69f5334f0693529c8a675d5dd35c
Lance Ivan Gil Fernandez <128129648+kenshiin1123@users.noreply.github.com>
2025-09-08 00:10:40 +0800

    Add chat autoscroll when there is new chat

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,20 +1,28 @@
 import React, { useContext } from "react";
 import type { ChatType, UserType } from "../types";
 import { ChatContext } from "../App";
+import { motion } from "motion/react";
+
 const Chat: React.FC<{ chat: ChatType }> = ({ chat }) => {
   const { state } = useContext(ChatContext);
   const userId: UserType["userId"] = state.user.userId;
+  const userChat = userId === chat.userId;
+
+  const animate = {
+    x: userChat ? [20, 0] : [-20, 0],
+    opacity: [0, 1],
+  };
 
   return (
-    <li
-      className={`chat ${userId !== chat.userId ? " chat-start" : "chat-end"}`}
-    >
-      <div className="chat-bubble">
+    <li className={`chat ${!userChat ? " chat-start" : "chat-end"}`}>
+      <motion.div
+        className={`chat-bubble`}
+        animate={animate}
+        transition={{ duration: 0.39 }}
+      >
         <h1>{chat.message}</h1>
-        {userId !== chat.userId && (
-          <p className="font-bold text-xs mt-2">{chat.sender}</p>
-        )}
-      </div>
+        {!userChat && <p className="font-bold text-xs mt-2">{chat.sender}</p>}
+      </motion.div>
     </li>
   );
 };
--- frontend/src/components/Chats.tsx
+++ frontend/src/components/Chats.tsx
@@ -1,11 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import Chat from "./Chat";
 import { ChatContext } from "../App";
 
 const Chats = () => {
   const { state } = useContext(ChatContext);
+  const containerRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const el = containerRef.current;
+    if (el) {
+      el.scrollTo({ top: el.scrollHeight, behavior: "smooth" });
+    }
+  }, [state.chats]);
+
   return (
-    <ul className="p-3 h-[85%] overflow-y-scroll space-y-4">
+    <ul ref={containerRef} className="p-3 h-[85%] overflow-y-scroll space-y-4">
       {state.chats.length < 1 && (
         <h1 className="text-black font-bold text-center animate-pulse">
           No chats available
